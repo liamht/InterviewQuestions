980d0dda8f854037dc112ad9ee518531df662df2
liamht <ljht93@hotmail.com>
2018-02-14 21:24:09 +0000

    Added question with a logic puzzle

---
+++ InterviewQuestions/Question 15 - Logic Puzzles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    /// <summary>
+    /// Without creating another variable, write code to swap around the values of firstNumber and secondNumber
+    /// </summary>
+    public class Question15
+    {
+        public void SwitchNumbers(int firstNumber, int secondNumber)
+        {
+            
+        }
+    }
+}
