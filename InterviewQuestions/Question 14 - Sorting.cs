a38c23ec1c284f472f10b8e10bde1e6246927c85
liamht <ljht93@hotmail.com>
2018-02-14 21:20:56 +0000

    Added question on sorting

---
+++ InterviewQuestions/Question 14 - Sorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    /// <summary>
+    /// Without using any kind of .Sort or OrderBy Method or touching the constructor (other than one you make yourself) re-arrange the Numbers list into numerical order
+    /// Hint: I'm looking for loops
+    /// </summary>
+    public class Question14
+    {
+        public List<int> Numbers { get; set; }
+
+        public Question14()
+        {
+            Numbers = new List<int>()
+            {
+                1,9,32,5,2,6,4,2,7
+            };    
+        }
+
+        public void Answer()
+        {
+            
+        }
+    }
+}
