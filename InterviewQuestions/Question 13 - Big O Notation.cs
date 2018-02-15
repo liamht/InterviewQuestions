e4505fcbf036dc9edcdb90d08257bec0c61b0c31
Liam Hayman Tansley <liam.haymantansley@openprojects.co.uk>
2018-02-15 08:58:15 +0000

    Changed typo in question

--- InterviewQuestions/Question 13 - Big O Notation.cs
+++ InterviewQuestions/Question 13 - Big O Notation.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace InterviewQuestions
 {
     /// <summary>
-    /// Write some code that could be described in Big O notation as O(3)
+    /// Write some code that could be described in Big O notation as O(n^3)
     /// </summary>
     public class Question13
     {
